c78ceb7751921961b0e4205a27882e1a1c97ed5e
hnakae <nakae27@gmail.com>
2019-01-19 20:09:45 -0800

    been a a while

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignupFormComponent } from './app/signup-form/signup-form.component';
+import { LoginFormComponent } from './app/login-form/login-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'signup', component: SignupFormComponent },
+  { path: 'login', component: LoginFormComponent },
+  
+  { path: '/', redirectTo: '/login', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ListComponent } from './list/list.component';
+import { DetailsComponent } from './details/details.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ListComponent,
+    DetailsComponent,
+    LoginFormComponent,
+    SignupFormComponent,
+    NavbarComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
